7800330deabd903ca013186ac5f3196600ebb570
Sebastian <sebastian.velo@mercadolibre.com>
2025-06-05 14:06:37 -0300

    refactor: update Header and ThemeToggleButton components for improved styling and consistency

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -3,7 +3,7 @@ import ThemeToggleButton from "./actions/ThemeToggleButton";
 
 const Header: React.FC = () => {
   return (
-    <header className="lg:px-12 flex justify-between items-center fixed top-0 z-50 backdrop-blur-xl w-full py-2 px-3 bg-white/50 text-secondary-900 dark:bg-black/50 dark:text-secondary-100 border-b border-secondary-900/20 dark:border-secondary-500/20 h-16">
+    <header className="lg:px-12 flex justify-between items-center fixed top-0 z-50 backdrop-blur-xl w-full py-2 px-3 bg-secondary-50/50 text-secondary-900 dark:bg-secondary-950/70 dark:text-secondary-100 border-b border-secondary-900/20 dark:border-secondary-500/20 h-16">
       <div className="flex group">
         <h1 className="text-xl md:text-2xl font-bold group-hover:tracking-tight transition-all duration-300 text-primary-900 dark:text-primary-100 font-stoke">Ide</h1>
         <h1 className="text-xl md:text-2xl font-bold group-hover:tracking-wider transition-all duration-300 text-primary-900 dark:text-primary-100 font-stoke">alRes</h1>
--- src/layout/header/actions/ThemeToggleButton.tsx
+++ src/layout/header/actions/ThemeToggleButton.tsx
@@ -5,7 +5,7 @@ const ThemeToggleButton: React.FC = () => {
     const { darkMode, toggleDarkMode } = useDarkMode();
 
     return (
-        <button onClick={toggleDarkMode} className="p-2 rounded-lg bg-secondary-200 dark:bg-secondary-800 hover:bg-secondary-300 dark:hover:bg-secondary-700 transition-colors focus:outline-none focus:ring-2 focus:ring-secondary-500" aria-label={darkMode ? "Switch to light mode" : "Switch to dark mode"}>
+        <button onClick={toggleDarkMode} className="p-2 bg-white hover:bg-white/50 hover:text-sky-500 text-secondary-700 focus:ring-secondary-300 dark:bg-black/80 dark:hover:bg-black/50 dark:text-secondary-300 dark:hover:text-yellow-200 rounded-full transition-all duration-200 focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed" aria-label={darkMode ? "Switch to light mode" : "Switch to dark mode"}>
             {darkMode ? <Icons.Sun /> : <Icons.Moon />}
         </button>
     );
